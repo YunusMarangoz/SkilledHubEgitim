61acb46174177bc449be65c0d0f2549a9abcc640
Yunus <yunusmarangoz@gmail.com>
2025-06-03 22:21:38 +0300

    sınıflar

---
+++ Konu07Donguler/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Konu07Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dönguler!");
+
+            Console.WriteLine("1-For Döngüsü");
+            for (int i = 0; i < 5; i++) //i değişkeni oluştur ve 0 değerini ata; i 5 den küçük olduğu sürece dön; her dönüşte i yi 1 artır i++ ile
+            {
+                Console.WriteLine("i değişkeninin değeri {0}", i);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("2-While Döngüsü");
+            int j = 0; // dışarıda bir değişken tanımlıyoruz
+            while (j < 5) //While döngüsünün şartı bu şekilde, anlamı toplam küçükse 5 den sürekli dön
+            {
+                Console.WriteLine("While Sayı {0} değeri", j);//toplamın değerini ekrana yazdırıyoruz
+                j++;//toplam sayısını arttırıyoruz ki sonsuz döngüye girmesin program
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("3-Do While Döngüsü");
+            int toplam = 7;
+            do
+            {
+                Console.WriteLine("toplamın değeri : " + toplam);
+            } while (toplam < 5);
+
+            Console.WriteLine();
+            string[] kategoriler={"Bilgisayar","Elektronik","Cep Telefonu"};
+
+            Console.WriteLine("4- Foreach Döngüsü");
+            Console.WriteLine("Kategoriler : ");
+            foreach (var kategori in kategoriler)// kategori (dizi ogesi) kategoriler dizsindeki her bir elemeana karşılık geliyor
+            { 
+                Console.WriteLine(kategori);
+            }
+            Console.WriteLine();
+
+            // İç içe Döngü Kullanımı
+
+            string[] urunler = { "Ürün 1", "Ürün 2", "Ürün 3" };
+            foreach (var kategori in kategoriler)
+            {
+                Console.WriteLine(kategori);
+                foreach (var urun in urunler)
+                {
+                    Console.WriteLine(urun);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("iç İçe For Döngüsü");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i değişkeninin değeri {i}");// Console.WriteLine("i değişkeninin değeri {0}", i); yazımları aynı
+                for (int k = 0; k < 3; k++)
+                {
+                    Console.WriteLine($"\t k değişkeninin değeri {k}");
+                }
+            }
+        }
+    }
+}
---
+++ Konu08SiniflarClasses/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Konu08SiniflarClasses
+{
+    internal class Ev//Sınıf Tanımlama
+    {
+        internal string SokakAdi;// internal erişim belirleyici
+        internal int KapiNo;
+    }
+    /*C# nesne yönelimli bir programlama dili olduğu için her şey sınıflar içinde tanımlanır.
+    sınıflara ve sınıf öğelerine erişim kısıtlanabilir veya belirli düzeylerde erişime izin verilebilir.
+    Öğelere erişimi kısıtlayan ya da yetki veren anahtar sözcüklere "Erişim Belirteçleri" (acces modifiers) denir.
+    *Erişim belirteçleri 4 ana sınıfa ayrılır
+    * public    : Erişim kısıtı yoktur, her yerden erişilebilir
+    * protected : Ait olduğu sınıftan ve o sınıftan türetilen sınıflardan erişilebilir
+    * internal  : Etkin projeye ait sınıflardan erişilebilir, onların dışında erişilemez
+    * private   : Yalnız bulunduğu sınıftan erişilebilir, dıştaki sınıflardan erişilemez
+    * Bir öğe yalnızca 1 erişim belirteci alabilir
+    * namespace erişim belirteci almaz çünkü o daima public tir
+    * Sınıflar public yada internal nitelemesi alabilirler ama protected yada private nitelemesi alamazlar
+    * enum erişim belirteci almaz çünkü daima public tir
+    */  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sınıflar - Classes");
+
+            Ev ilkev = new Ev();// soyut bir yapı olan ev sınıfından, somut bir nesne olan ilkev i oluşturduk
+            ilkev.SokakAdi = "Çiçek SK";
+            ilkev.KapiNo = 10;
+            Console.WriteLine("İlkEv Sokak Adı : "+ilkev.SokakAdi);
+            Console.WriteLine("İlkEv Kapi No : "+ilkev.KapiNo);
+
+            Console.WriteLine();
+            
+            Ev yazlikEv = new Ev();
+            yazlikEv.SokakAdi = "Deniz SK";
+            yazlikEv.KapiNo = 15;
+
+            Console.WriteLine("Yazlikev Sokak Adı : " + yazlikEv.SokakAdi);
+            Console.WriteLine("Yazlik Kapi No : " + yazlikEv.KapiNo);
+
+            Console.WriteLine();
+           
+            Ev koyevi = new()
+            {
+                SokakAdi = "Pınar SK",// bu kullamımda ; yerine öğeler arasına , koymanız gerekli
+                KapiNo = 34
+            };
+            
+        }
+    }
+}
